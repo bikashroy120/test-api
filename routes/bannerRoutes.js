8c98dfbb63add2aec8f247ad4b7fb00f0c51d2f4
bikashroy120 <bikashroydt@gmail.com>
2024-02-16 14:02:20 +0600

    banner delete add

--- routes/bannerRoutes.js
+++ routes/bannerRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const {authMiddleware,isAdmin} = require("../middlewarer/authMiddlewarer");
-const { createBanner, getBanner } = require("../conttoller/bannerConttroller");
+const { createBanner, getBanner, deleteBanner } = require("../conttoller/bannerConttroller");
 
 
 
 
-router.post("/",createBanner)
+router.post("/",authMiddleware,isAdmin,createBanner)
 router.get("/all-banner",getBanner)
-router.delete("/delete-banner/:id",getBanner)
+router.delete("/delete-banner/:id",authMiddleware,isAdmin,deleteBanner)
 
 
 module.exports=router;
